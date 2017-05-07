8430131bf4ffa5488c3c999e6c07c594777886f0
Yi Zhao <zhaoyi0113@gmail.com>
2017-05-07 20:21:44 +1000

    update hiave 3-2

--- hive-3-2.sql
+++ hive-3-2.sql
@@ -1,13 +1,12 @@
 
-select  distinct( h.name )TeamName, 
-    m.firstname FirstName, m.lastname LastName, t.year Year, 
-	t.pts Point, t.goals Goals, t.assists Assists, m.playerId
-from 
-(select t.pts pts, t.year year, t.goals goals, t.assists assists, s.playerId, t.tmid from 
+select s.name,s.firstName,s.lastName,s.pts, s.year, s.pts, s.g,s.a from (
+    select t.name, m.firstName, m.lastName, s.year,s.pts, s.g, s.a from 
+    (
+    select s.playerId, s.tmId, s.pts, s.g, s.a, s.year FROM
     (select max(s.pts) pts, s.tmid from  scoring s group by s.tmid) t
-    join scoring s on s.pts = t.pts and s.year = t.year and s.g = t.goals and s.a = t.assists
-) t
-join master m on (m.playerId = t.playerId)
-join ice_hockey_teams h on h.tmid = t.tmid
-where h.name = 'Czechoslovakia'
-order by TeamName;
+    join scoring s 
+    on s.pts = t.pts and s.tmid = t.tmid
+    order by tmid 
+    ) s join master m on s.playerId = m.playerId
+    join ice_hockey_teams t on t.tmId = s.tmId 
+) s group by s.name,s.firstName,s.lastName,s.pts, s.year, s.pts, s.g,s.a
