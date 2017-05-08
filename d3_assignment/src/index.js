533f76fafe81e92d0cb5e9c8f7a3b98d34d4360f
Yi Zhao <zhaoyi0113@gmail.com>
2017-05-08 21:54:57 +1000

    tooltip on line

--- d3_assignment/src/index.js
+++ d3_assignment/src/index.js
@@ -26,9 +26,7 @@ function drawCircle(data) {
     );
   var tip = d3.tip().attr("class", "d3-tip").offset([-10, 0]).html(function(d) {
     console.log("show tip ", d);
-    return (
-      "<strong>Frequency:</strong> <span style='color:red'>" + d.id + "</span>"
-    );
+    return "<strong>Site:</strong> <span style='color:red'>" + d.id + "</span>";
   });
   var circle = svg.selectAll("circle").data(data.nodes);
   svg.call(tip);
@@ -38,25 +36,66 @@ function drawCircle(data) {
     return d.y;
   });
   circleEnter.attr("cx", function(d, i) {
-    return i * 100 + 30;
+    return d.x;
   });
   circleEnter.attr("r", function(d) {
-    return Math.sqrt(20);
+    return Math.sqrt(50);
   });
-  circleEnter.on("mouseover", tip.show);
+  circleEnter.on("mouseover", tip.show).on("mouseout", tip.hide);
 
-  drawLine(svg,data);
+  drawLine(svg, data);
 }
 
 function drawLine(svg, data) {
-    d3.select(".svg-circle").data(data.nodes)
+  var links = data["links"].map(function(link) {
+    var node1 = data["nodes"].filter(function(node) {
+      return node.id == link.node01;
+    })[0];
+    var node2 = data["nodes"].filter(function(node) {
+      return node.id == link.node02;
+    })[0];
+    return {
+      x1: node1.x,
+      x2: node2.x,
+      y1: node1.y,
+      y2: node2.y,
+      amount: link.amount,
+      n1: link.node01,
+      n2: link.node02
+    };
+  });
+  var tip = d3.tip().attr("class", "d3-tip").offset([-10, 0]).html(function(d) {
+    console.log("show tip ", d);
+    return (
+      "<div style='color:red'><strong>Site1:</strong><span>" +
+      d.n1 +
+      "</span><br><strong>Site2:</strong><span>" +
+      d.n2 +
+      "</span>"+"<br><strong>Amount:</strong><span>"+d.amount+ "</span></div>"
+    );
+  });
+  svg.call(tip);
+  svg
+    .selectAll("line")
+    .data(links)
+    .enter()
     .append("line")
-    .attr("x1", function(d){console.log('dare:',d);return d.x;})
-    .attr("y1", function(d){return d.y;})
-    .attr("x2", function(d){return d.x+50;})
-    .attr("y2", function(d){return d.y+50;})
+    .attr("x1", function(d) {
+      return d.x1;
+    })
+    .attr("y1", function(d) {
+      return d.y1;
+    })
+    .attr("x2", function(d) {
+      return d.x2;
+    })
+    .attr("y2", function(d) {
+      return d.y2;
+    })
     .attr("stroke-width", 2)
-    .attr("stroke", "black");
+    .attr("stroke", "black")
+    .on("mouseover", tip.show)
+    .on("mouseout", tip.hide);
 }
 
 loadData();
