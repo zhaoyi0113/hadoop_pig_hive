ff5241666439f6db3dbe786b513dc989dd335254
Yi Zhao <zhaoyi0113@gmail.com>
2017-05-08 20:28:19 +1000

    initialize express server

---
+++ d3_assignment/src/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require('path');
+var app = express();
+
+app.use("/public", express.static("public"));
+app.use("/", express.static('./'));
+app.use(express.static('./'));
+
+app.listen(3333, function() {
+  console.log("D3 Virtualization app listening on port 3333!");
+});
