61d43fd9f78975fe105a4c3fd71affa522abcdea
Yi Zhao <zhaoyi0113@gmail.com>
2017-05-14 12:45:21 +1000

    update

--- pm25/data_analyst.R
+++ pm25/data_analyst.R
@@ -1,60 +1,15 @@
 library(stringr)
 library(gdata)
 library(plyr)
-library('lubridate')
+library(lubridate)
 library(plyr)
 library(dplyr)
 
-path <- '../beijing_20160101-20161231/'
-
-changeColumnName <- function(data) {
-  data = rename.vars(data, from = '东四', to = 'DongSi')
-  data = rename.vars(data, from = '天坛', to = 'TianTan')
-  data = rename.vars(data, from = '官园', to = 'GuanYuan')
-  data = rename.vars(data, from = '万寿西宫', to = 'WanShouXiGong')
-  data = rename.vars(data, from = '奥体中心', to = 'AoTiZhongXin')
-  data = rename.vars(data, from = '农展馆', to = 'NongZhanGuan')
-  data = rename.vars(data, from = '万柳', to = 'WanLiu')
-  data = rename.vars(data, from = '北部新区', to = 'BeibuXinqu')
-  data = rename.vars(data, from = '植物园', to = 'ZhiWuYuan')
-  data = rename.vars(data, from = '丰台花园', to = 'FengTaiHuaYuan')
-  data = rename.vars(data, from = '云岗', to = 'YunGang')
-  data = rename.vars(data, from = '古城', to = 'GuCheng')
-  data = rename.vars(data, from = '房山', to = 'FangShan')
-  data = rename.vars(data, from = '大兴', to = 'DaXing')
-  data = rename.vars(data, from = '顺义', to = 'ShunYi')
-  data = rename.vars(data, from = '亦庄', to = 'YiZhuang')
-  data = rename.vars(data, from = '通州', to = 'TongZhou')
-  data = rename.vars(data, from = '昌平', to = 'ChangPing')
-  data = rename.vars(data, from = '门头沟', to = 'MenTouGou')
-  data = rename.vars(data, from = '平谷', to = 'PingGu')
-  data = rename.vars(data, from = '怀柔', to = 'HuaiRou')
-  data = rename.vars(data, from = '密云', to = 'MiYun')
-  data = rename.vars(data, from = '延庆', to = 'YanQing')
-  data = rename.vars(data, from = '定陵', to = 'DingLing')
-  data = rename.vars(data, from = '八达岭', to = 'BaDaLin')
-  data = rename.vars(data, from = '密云水库', to = 'MiYunShuiKu')
-  data = rename.vars(data, from = '东高村', to = 'DongGaoCun')
-  data = rename.vars(data, from = '永乐店', to = 'YongLeDian')
-  #data = rename.vars(data, from = '东高村', to = 'DongGaoCun')
-  data = rename.vars(data, from = '榆垡', to = 'YuFa')
-  data = rename.vars(data, from = '琉璃河', to = 'LiuLiHe')
-  data = rename.vars(data, from = '前门', to = 'QianMen')
-  data = rename.vars(data, from = '永定门内', to = 'YongDingMenNei')
-  data = rename.vars(data, from = '西直门北', to = 'XiZhiMenBei')
-  data = rename.vars(data, from = '南三环', to = 'NanSanHuan')
-  data = rename.vars(data, from = '东四环', to = 'DongSiHuan')
-  
-}
+source('raw_data.R')
+loadData('2016')
 
 querySingleDayData <- function(date) {
-  fileName <-
-    str_c(path, 'beijing_all_', format(date, "%Y%m%d"), '.csv')
-  print(str_c('load file ', fileName))
-  data <- read.csv(fileName, header = TRUE)
-  data[is.na(data)] <- 0
-  data <- changeColumnName(data)
-  split(data, data$type)
+  getDataByDate(date)
 }
 
 queryDate <- function(start, end) {
@@ -92,12 +47,12 @@ getMeanValueOnEachKPI <- function(datas) {
       }
     }
   }
-  total <- total[, !(names(total) %in% c('date', 'hour', 'type'))]
+  total <- total[,!(names(total) %in% c('date', 'hour', 'type'))]
   total <- as.list(colMeans(total))
 }
 
 # read data with the date period
-queryDataWithSum <- function(start, end, kpi) {
+queryDataWithRange <- function(start, end, kpi) {
   startDate <- start
   endDate <- end
   if (typeof(startDate) == 'character') {
@@ -120,16 +75,10 @@ queryDataWithSum <- function(start, end, kpi) {
     if (is.na(kpiDF)) {
       kpiDF <- aqi[-c(1:3)]
     } else {
-      kpiDF <- kpiDF + aqi[-c(1:3)]
+      kpiDF <- rbind(kpiDF , aqi[-c(1:3)])
     }
-    
-    #kpiDF <- ddply(merge(kpiDF, aqi, all=TRUE), .('date', 'hour', 'type'), summarise, )
-    
-    
-    #kpiDF[is.na(kpiDF)] <- 0
     kpiMean <- sapply(kpiDF, FUN = mean)
     kpiDF <- kpiMean
-    
     date <- date + 1
   }
   sub <- data.frame(Date = startDate, type = kpi)
@@ -152,7 +101,7 @@ queryDataForMonth <-
     } else{
       endDate <- as.Date(str_c(year, '-12-31'))
     }
-    queryDataWithSum(startDate, endDate, kpi)
+    queryDataWithRange(startDate, endDate, kpi)
   }
 
 # group data by month
@@ -167,23 +116,23 @@ queryDataForYear <-
         md <- NA
         md <- queryDataForQuarter(year, str_c(m), kpi)
         if (is.na(monthData)) {
-          monthData <- md[0, ]
+          monthData <- md[0,]
         }
-        monthData[nrow(monthData) + 1, ] <- md[1, ]
+        monthData[nrow(monthData) + 1,] <- md[1,]
       }
-    } else if(category == 'YEAR'){
-      startDate <- str_c(year,'-01-01')
-      endDate <- str_c(year,'-12-31')
-      monthData <- queryDataWithSum(startDate, endDate, kpi)
-    } 
+    } else if (category == 'YEAR') {
+      startDate <- str_c(year, '-01-01')
+      endDate <- str_c(year, '-12-31')
+      monthData <- queryDataWithRange(startDate, endDate, kpi)
+    }
     else {
       for (m in c(rep(1:12))) {
         md <- NA
         md <- queryDataForMonth(year, m, kpi)
         if (is.na(monthData)) {
-          monthData <- md[0, ]
+          monthData <- md[0,]
         }
-        monthData[nrow(monthData) + 1, ] <- md[1, ]
+        monthData[nrow(monthData) + 1,] <- md[1,]
       }
     }
     monthData
@@ -209,5 +158,5 @@ queryDataForQuarter <-
       endDate <- as.Date(str_c(year, '-12-31'))
     }
     print(str_c('query data ', startDate, endDate, kpi))
-    queryDataWithSum(startDate, endDate, kpi)
+    queryDataWithRange(startDate, endDate, kpi)
   }
